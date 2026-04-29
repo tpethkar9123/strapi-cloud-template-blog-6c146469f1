0544b237a443d10077f5efff53b79d7d1535fab3
Tushar Pethkar <tusharpethkar9123@gmail.com>
2026-04-29 18:14:20 +0530

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -14,4 +14,26 @@ module.exports = ({ env }) => ({
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
+  preview: {
+    enabled: true,
+    config: {
+      allowedOrigins: env('CLIENT_URL'), // Usually http://localhost:3000
+      async handler(uid, { documentId, locale, status }) {
+        const document = await strapi.documents(uid).findOne({ documentId });
+        
+        // This logic tells Strapi which URL to open
+        let path = "/";
+        if (uid === "api::article.article") path = `/blog/${document.slug}`;
+        if (uid === "api::about.about") path = "/about";
+
+        const urlSearchParams = new URLSearchParams({
+          url: path,
+          secret: env('PREVIEW_SECRET'), // A password you create
+          status,
+        });
+
+        return `${env('CLIENT_URL')}/api/preview?${urlSearchParams}`;
+      },
+    },
+  },
 });
